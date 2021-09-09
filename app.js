4ad307fc525953a965b6c06dca79bc83114f3ad5
PatKo1010 <Pat19951010>
2021-09-09 22:52:00 +0800

    download handlebars

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get ('/', (req,res) => {
   res.send ('This is a restaurant list')
 })
+
 
 app.listen (port, () => {
   console.log (`Express is running on http://locolhost:${port}`)
