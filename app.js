9c55be411c1cdc15c0c573d5cdf4739bb66d10cf
PatKo1010 <Pat19951010>
2021-09-12 00:27:31 +0800

    建立查詢以及點選清單的功能

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const restaurants = require('./restaurant.json')
+
 const exphbs = require('express-handlebars')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -10,7 +12,24 @@ app.set('view engine', 'handlebars')
 app.use (express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {restaurants: restaurants.results})
+})
+
+
+app.get ('/restaurants/:id', (req,res) => {
+  const id = Number(req.params.id)
+  const restaurant = restaurants.results.filter ( (element) => { 
+    return Number (element.id) === id 
+  })
+  res.render ('show', {restaurant: restaurant[0]})
+})
+
+app.get ('/search', (req,res) => {
+  const keyword = req.query.keyword
+  const searchedRestaurants = restaurants.results.filter ((element) => {
+    return element.name.toLowerCase().replace(/\s*/g, "").includes(req.query.keyword.toLowerCase().replace(/\s*/g, "")) ||element.category.toLowerCase().includes (req.query.keyword.toLowerCase())
+  })
+  res.render ('index', {restaurants: searchedRestaurants, keyword: keyword})
 })
 
 
