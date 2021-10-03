30aa56c82bb31b78aedeb49687e2c8a5c7b37a57
PatKo1010 <Pat19951010>
2021-10-03 21:06:45 +0800

    add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,13 +5,25 @@ const storeList = require ('../../models/restaurant')
 
 
 router.get('/', (req, res) => {
-  storeList.find()
-    .lean()
-    .sort({ _id: 'desc' })
-    .then((store) => {
-      res.render('index', { restaurants: store })
-    })
-    .catch(error => console.log(error))
+  const sort = req.query.sort
+  const sortMethods = {
+    'a-z': {name:"asc"},
+    'z-a': {name: "desc"},
+    'location':{location: 'asc'},
+    'rating': {rating : 'desc'},
+    'category' : {category : 'asc'}
+  } 
+ 
+  const sortValue = sort ? { [sort]: true } : { "a-z": true }
+
+    storeList.find()
+      .lean()
+      .sort(sortMethods[sort]|| {name:'asc'})
+      .then((store) => {
+        res.render('index', { restaurants: store, sortValue })
+      })
+      .catch(error => console.log(error))
+
 })
 
 module.exports = router
